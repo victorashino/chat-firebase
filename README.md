d3a0a21cd907fcbb7368d5cea126feb8b392ed3b
Victor Barbosa Ashino <106106717+victorashino@users.noreply.github.com>
2024-08-17 15:59:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Chat com Realtime Database
+
+**Descrição:**
+
+Chat DevSpace é um aplicativo que permite aos usuários fazerem login e conversarem dentro de um Chat. 
+Ele foi criado utilizando o Realtime Database, uma ferramenta do Firebase, que nos permite guardar e recuperar informações em tempo real.
+
+**Tecnologias:**
+
+* Kotlin
+* Jetpack Compose
+* Compose Navigation
+* Firebase
+* Realtime Database
+* Gradle
+* SharedPreferences
+
+**Screenshots:**
+
+Para utilizar o aplicativo você deve informar as seguintes credenciais para diferencias os usuários:
+
+Usuário: userone
+Senha: [QualquerUma]
+
+Usuário: other
+Senha: [QualquerUma]
+
+<img src="https://github.com/user-attachments/assets/06826968-f8a2-4a30-ac7a-7ee6a104b75b" width=260/>
+<img src="https://github.com/user-attachments/assets/dac7d616-fc69-459e-ba8f-26cfbf0e7f65" width=260/>
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.pedromoura.chatfirebase"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.pedromoura.chatfirebase"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
